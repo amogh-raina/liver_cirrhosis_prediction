88146753edf1a41a1518cdf469dee3400f4569d4
Amogh Raina <amoghraina204@gmail.com>
2022-09-17 03:54:36 +0530

    web app is ready

--- app.py
+++ app.py
@@ -24,5 +24,15 @@ def predict_api():
     response  = jsonify(int(output[0]))
     return response
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted stage is {}".format(output))
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
