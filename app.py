0e66d60fb004f27648c9b0b75184fff7ed02a4e3
Amogh Raina <amoghraina204@gmail.com>
2022-09-17 03:29:40 +0530

    This commit is for web app

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import Flask,render_template,request,app,jsonify,url_for
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+## Load the model
+model = pickle.load(open('model.pkl','rb'))
+scaler = pickle.load(open('scaler.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
+    data = request.json['data']
+    print(data)
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    response  = jsonify(int(output[0]))
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
